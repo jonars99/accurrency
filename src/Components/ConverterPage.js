0abe8ebf06bec95373ff233caab535338c5b5ba5
Jo <88668777+jonars99@users.noreply.github.com>
2022-01-12 15:43:23 +0000

    simple one way conversion set up between jpy-gbp example

--- src/Components/ConverterPage.js
+++ src/Components/ConverterPage.js
@@ -1,6 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { checkStatus, json } from './utils';
 
 const ConverterPage = () => {
+
+  // states for input/output currency selection
+  const [input, setInput] = useState('1');
+  const [output, setOutput] = useState('');
+
+  const handleInput = (event) => {
+    setInput(event.target.value);
+  }
+
+  useEffect(() => {
+    fetch(`https://altexchangerateapi.herokuapp.com/latest?amount=${input}&from=JPY&to=GBP`)
+    .then(checkStatus)
+    .then(json)
+    .then((data) => {
+      setOutput(data.rates.GBP);
+    })
+  })
+
   return(
     <div className="container-fluid">
       <div className="row p-5 converter">
@@ -10,7 +29,7 @@ const ConverterPage = () => {
 
               <div className="col-5">
                 <p>from</p>
-                <input className="form-control my-3" type="number" min="1"></input>    
+                <input value={input} onChange={handleInput} className="form-control my-3" type="number" min="1"></input>    
                 <select className="form-select">
                   <option value="jpy">JPY</option>
                   <option value="gbp">GBP</option>
@@ -25,7 +44,7 @@ const ConverterPage = () => {
 
               <div className="col-5">
                 <p>to</p>
-                <input className="form-control my-3" type="number" min="1"></input>
+                <input value={output} className="form-control my-3" type="number" min="1"></input>
                 <select className="form-select">
                   <option value="gbp">GBP</option>
                   <option value="jpy">JPY</option>
