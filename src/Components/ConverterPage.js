05ff757b960cb34a438990a4277a2db575a550ff
Jo <88668777+jonars99@users.noreply.github.com>
2022-01-13 11:08:19 +0000

    separate currencies to component

--- src/Components/ConverterPage.js
+++ src/Components/ConverterPage.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { checkStatus, json } from './utils';
+import { checkStatus, json, Options } from './utils';
 
 const ConverterPage = () => {
 
@@ -60,41 +60,7 @@ const ConverterPage = () => {
               <div className="col-5">
                 <p>from</p>
                 <input value={input} onChange={handleInput} className="form-control my-3" type="number" min="1"></input>    
-                <select name="currencies-in" className="form-select" onChange={handleCurrencyInput} defaultValue="GBP">
-                  <option value="AUD">AUD - Australian Dollar</option>
-                  <option value="BGN">BGN - Bulgarian Lev</option>
-                  <option value="BRL">BRL - Brazilian Real</option>
-                  <option value="CAD">CAD - Canadian Dollar</option>
-                  <option value="CHF">CHF - Swiss Franc</option>
-                  <option value="CNY">CNY - Chinese Renminbi Yuan</option>
-                  <option value="CZK">CZK - Czech Koruna</option>
-                  <option value="DKK">DKK - Danish Krone</option>
-                  <option value="EUR">EUR - Euro</option>
-                  <option value="GBP">GBP - British Pound</option>
-                  <option value="HKD">HKD - Hong Kong Dollar</option>
-                  <option value="HRK">HRK - Croatian Kuna</option>
-                  <option value="HUF">HUF - Hungarian Forint</option>
-                  <option value="IDR">IDR - Indonesian Rupiah</option>
-                  <option value="ILS">ILS - Israeli New Sheqel</option>                  
-                  <option value="INR">INR - Indian Rupee</option>
-                  <option value="ISK">ISK - Icelandic Króna</option>
-                  <option value="JPY">JPY - Japanese Yen</option>
-                  <option value="KRW">KRW - South Korean Won</option>
-                  <option value="MXN">MXN - Mexican Peso</option>
-                  <option value="MYR">MYR - Malaysian Ringgit</option>
-                  <option value="NOK">NOK - Norwegian Krone</option>
-                  <option value="NZD">NZD - New Zealand Dollar</option>
-                  <option value="PHP">PHP - Philippine Peso</option>
-                  <option value="PLN">PLN - Polish Złoty</option>
-                  <option value="RON">RON - Romanian Leu</option>
-                  <option value="RUB">RUB - Russian Ruble</option>
-                  <option value="SEK">SEK - Swedish Krona</option>
-                  <option value="SGD">SGD - Singapore Dollar</option>
-                  <option value="THB">THB - Thai Baht</option>
-                  <option value="TRY">TRY - Turkish Lira</option>
-                  <option value="USD">USD - United States Dollar</option>
-                  <option value="ZAR">ZAR - South African Rand</option>
-                </select>      
+                <Options handleCurrencyInput={handleCurrencyInput} name='currencies-in' />
               </div>
 
               <div className="col-2 d-flex justify-content-center align-items-center">
@@ -104,41 +70,7 @@ const ConverterPage = () => {
               <div className="col-5">
                 <p>to</p>
                 <p>{output}</p>
-                <select className="form-select" name="currencies-out" onChange={handleCurrencyOutput} defaultValue='JPY' >
-                  <option value="AUD">AUD - Australian Dollar</option>
-                  <option value="BGN">BGN - Bulgarian Lev</option>
-                  <option value="BRL">BRL - Brazilian Real</option>
-                  <option value="CAD">CAD - Canadian Dollar</option>
-                  <option value="CHF">CHF - Swiss Franc</option>
-                  <option value="CNY">CNY - Chinese Renminbi Yuan</option>
-                  <option value="CZK">CZK - Czech Koruna</option>
-                  <option value="DKK">DKK - Danish Krone</option>
-                  <option value="EUR">EUR - Euro</option>
-                  <option value="GBP">GBP - British Pound</option>
-                  <option value="HKD">HKD - Hong Kong Dollar</option>
-                  <option value="HRK">HRK - Croatian Kuna</option>
-                  <option value="HUF">HUF - Hungarian Forint</option>
-                  <option value="IDR">IDR - Indonesian Rupiah</option>
-                  <option value="ILS">ILS - Israeli New Sheqel</option>                  
-                  <option value="INR">INR - Indian Rupee</option>
-                  <option value="ISK">ISK - Icelandic Króna</option>
-                  <option value="JPY">JPY - Japanese Yen</option>
-                  <option value="KRW">KRW - South Korean Won</option>
-                  <option value="MXN">MXN - Mexican Peso</option>
-                  <option value="MYR">MYR - Malaysian Ringgit</option>
-                  <option value="NOK">NOK - Norwegian Krone</option>
-                  <option value="NZD">NZD - New Zealand Dollar</option>
-                  <option value="PHP">PHP - Philippine Peso</option>
-                  <option value="PLN">PLN - Polish Złoty</option>
-                  <option value="RON">RON - Romanian Leu</option>
-                  <option value="RUB">RUB - Russian Ruble</option>
-                  <option value="SEK">SEK - Swedish Krona</option>
-                  <option value="SGD">SGD - Singapore Dollar</option>
-                  <option value="THB">THB - Thai Baht</option>
-                  <option value="TRY">TRY - Turkish Lira</option>
-                  <option value="USD">USD - United States Dollar</option>
-                  <option value="ZAR">ZAR - South African Rand</option>
-                </select>
+                <Options handleCurrencyOutput={handleCurrencyOutput} name="currencies-out" />
               </div>
 
             </div>
--- src/Components/utils.js
+++ src/Components/utils.js
@@ -6,3 +6,55 @@ export const checkStatus = (response) => {
 }
 
 export const json = (response) => response.json();
+
+export const Options = (props) => {
+  let type;
+  let defaultVal;
+  if (props.name === "currencies-in") {
+    type = props.handleCurrencyInput;
+    defaultVal = 'GBP'
+  }
+  else if (props.name === "currencies-out") {
+    type = props.handleCurrencyOutput;
+    defaultVal = 'JPY'
+  }
+
+  return(
+    <select className="form-select" name={props.name} onChange={type} defaultValue={defaultVal} >              
+      <option value="AUD">AUD - Australian Dollar</option>
+      <option value="BGN">BGN - Bulgarian Lev</option>
+      <option value="BRL">BRL - Brazilian Real</option>
+      <option value="CAD">CAD - Canadian Dollar</option>
+      <option value="CHF">CHF - Swiss Franc</option>
+      <option value="CNY">CNY - Chinese Renminbi Yuan</option>
+      <option value="CZK">CZK - Czech Koruna</option>
+      <option value="DKK">DKK - Danish Krone</option>
+      <option value="EUR">EUR - Euro</option>
+      <option value="GBP">GBP - British Pound</option>
+      <option value="HKD">HKD - Hong Kong Dollar</option>
+      <option value="HRK">HRK - Croatian Kuna</option>
+      <option value="HUF">HUF - Hungarian Forint</option>
+      <option value="IDR">IDR - Indonesian Rupiah</option>
+      <option value="ILS">ILS - Israeli New Sheqel</option>                  
+      <option value="INR">INR - Indian Rupee</option>
+      <option value="ISK">ISK - Icelandic Króna</option>
+      <option value="JPY">JPY - Japanese Yen</option>
+      <option value="KRW">KRW - South Korean Won</option>
+      <option value="MXN">MXN - Mexican Peso</option>
+      <option value="MYR">MYR - Malaysian Ringgit</option>
+      <option value="NOK">NOK - Norwegian Krone</option>
+      <option value="NZD">NZD - New Zealand Dollar</option>
+      <option value="PHP">PHP - Philippine Peso</option>
+      <option value="PLN">PLN - Polish Złoty</option>
+      <option value="RON">RON - Romanian Leu</option>
+      <option value="RUB">RUB - Russian Ruble</option>
+      <option value="SEK">SEK - Swedish Krona</option>
+      <option value="SGD">SGD - Singapore Dollar</option>
+      <option value="THB">THB - Thai Baht</option>
+      <option value="TRY">TRY - Turkish Lira</option>
+      <option value="USD">USD - United States Dollar</option>
+      <option value="ZAR">ZAR - South African Rand</option>
+    </select>
+  )}
+
+export default Options;
